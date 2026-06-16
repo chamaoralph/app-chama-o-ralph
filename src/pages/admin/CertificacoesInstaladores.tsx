becce0009cd237d488eeebc92a796b8d2d9bdd92
Rafael <chamaoralph@gmail.com>
2026-06-16 14:14:38 -0300

    Fix: remover !inner join em certificacoes para mostrar todos os registros

--- src/pages/admin/CertificacoesInstaladores.tsx
+++ src/pages/admin/CertificacoesInstaladores.tsx
@@ -22,7 +22,7 @@ export default function CertificacoesInstaladores() {
         .from('certificacoes')
         .select(`
           *,
-          instalador:usuarios!inner (
+          instalador:usuarios (
             nome,
             id
           ),
